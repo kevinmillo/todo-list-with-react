86a558f23b527105735a536407eaf88d4f3a44fd
kevinmillo <kbullor@hotmail.com>
2021-12-22 21:29:38 +0000

    subo avances

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { element } from "prop-types";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [todo, setTodo] = useState([]);
+	const agregartask = () => {
+		if (task !== "") {
+			setTodo([...todo, task]);
+			setTask("");
+		}
+	};
+	const eliminartask = index => {
+		console.log(index);
+		let todofiltrados = todo.filter((element, i) => {
+			if (index !== i) {
+				return element;
+			}
+		});
+		setTodo(todofiltrados);
+		//estilo
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h2>todos</h2>
+			<div class="row g-3 align-items-center">
+				<div class="col-8">
+					<input
+						className="form-control"
+						type="text"
+						onChange={e => setTask(e.target.value)}
+						value={task}
+					/>
+				</div>
+				<div class="col-4">
+					<button
+						className="btn btn-outline-dark w-100"
+						onClick={agregartask}>
+						add
+					</button>
+				</div>
+			</div>
+
+			<ul>
+				{todo.map((item, index) => (
+					<li key={index}>
+						{item}{" "}
+						<span onClick={() => eliminartask(index)}>x</span>
+					</li>
+				))}
+			</ul>
+			{todo.length > 0 && <span>tareas por hacer: {todo.length}</span>}
+			{todo.length == 0 && <span>No hay tareas por hacer</span>}
 		</div>
 	);
 };
